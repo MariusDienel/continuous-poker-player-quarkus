f44e41915c1ecad5d5ed288a7c689c9c099f1a39
mdienel <marius.dienel@doubleslash.de>
2022-07-22 20:43:35 +0200

    Test

--- src/main/java/de/doubleslash/poker/player/data/Rank.java
+++ src/main/java/de/doubleslash/poker/player/data/Rank.java
@@ -5,24 +5,26 @@ import java.util.Arrays;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum Rank {
-   ACE("A"),
-   KING("K"),
-   QUEEN("Q"),
-   JACK("J"),
-   TEN("10"),
-   NINE("9"),
-   EIGHT("8"),
-   SEVEN("7"),
-   SIX("6"),
-   FIVE("5"),
-   FOUR("4"),
-   THREE("3"),
-   TWO("2");
+   ACE("A", 5),
+   KING("K", 4),
+   QUEEN("Q", 4),
+   JACK("J", 3),
+   TEN("10", 3),
+   NINE("9", 2),
+   EIGHT("8", 2),
+   SEVEN("7", 2),
+   SIX("6", 2),
+   FIVE("5",1),
+   FOUR("4",1),
+   THREE("3",1),
+   TWO("2",1);
 
    private final String token;
+   private final int value;
 
-   Rank(final String token) {
+   Rank(final String token, int value) {
       this.token = token;
+      this.value = value;
    }
 
    public String getToken() {
@@ -33,5 +35,9 @@ public enum Rank {
    public static Rank forToken(final String token) {
       return Arrays.stream(Rank.values()).filter(r -> r.getToken().equals(token)).findFirst()
             .orElseThrow(() -> new IllegalArgumentException("Token not allowed: " + token));
+   }
+
+   public int getValue() {
+      return value;
    }
 }
--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -1,12 +1,32 @@
 package de.doubleslash.poker.player.logic;
 
+import de.doubleslash.poker.player.data.Card;
+import de.doubleslash.poker.player.data.Rank;
 import de.doubleslash.poker.player.data.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Strategy {
 
    public int decide(final Table table) {
       System.out.println(table);
-      return 0;
+      List<Card> cards = table.getOwnPlayer().getCards();
+      List<Card> communityCards = table.getCommunityCards();
+
+      List<Card> combined = new ArrayList<>(cards);
+      combined.addAll(communityCards);
+
+      List<Rank> ranks = combined.stream().map(Card::getRank).collect(Collectors.toList());
+
+      int bet = 0;
+      for (Rank rank : Rank.values()) {
+         int sum = ranks.stream().filter(r -> r.equals(rank)).mapToInt(r -> r.getValue()).sum();
+         bet += sum * rank.getValue();
+      }
+
+      return bet;
    }
 
 }
